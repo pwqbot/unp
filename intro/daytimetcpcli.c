68427e8422c1dccf4a3726202aeaa4a50b7a080b
pwqbot <ding00000804@gmail.com>
2022-10-26 15:12:35 +0800

    ch1

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -1,32 +1,42 @@
 #include "unp.h"
 
 int main(int argc, char **argv) {
-    int                sockfd, n;
+    int                sockfd = 0;
+    ssize_t n = 0;
     char               recvline[MAXLINE + 1];
     struct sockaddr_in servaddr;
 
-    if (argc != 2)
+    if (argc != 2) {
         err_quit("usage: a.out <IPaddress>");
+    }
 
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        err_sys("socket error");
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        err_sys("socket error %d", errno);
+    }
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port   = htons(13); /* daytime server */
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
         err_quit("inet_pton error for %s", argv[1]);
-
-    if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) < 0)
-        err_sys("connect error");
-
-    while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
-        recvline[n] = 0; /* null terminate */
-        if (fputs(recvline, stdout) == EOF)
-            err_sys("fputs error");
     }
-    if (n < 0)
-        err_sys("read error");
 
+    if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) < 0) {
+        err_sys("connect error");
+    }
+
+    int count = 0;
+    while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
+        count++;
+        recvline[n] = 0; /* null terminate */
+        if (fputs(recvline, stdout) == EOF) {
+            err_sys("fputs error");
+        }
+    }
+    if (n < 0) {
+        err_sys("read error");
+    }
+
+    printf("count: %d", count);
     exit(0);
 }
--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -1,32 +1,32 @@
-#include	"unp.h"
-#include	<time.h>
+#include "unp.h"
+#include <time.h>
 
-int
-main(int argc, char **argv)
-{
-	int					listenfd, connfd;
-	struct sockaddr_in	servaddr;
-	char				buff[MAXLINE];
-	time_t				ticks;
+int main(int argc, char **argv) {
+    int                listenfd = 0, connfd = 0;
+    struct sockaddr_in servaddr;
+    char               buff[MAXLINE];
+    time_t             ticks = 0;
 
-	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family      = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(13);	/* daytime server */
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family      = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port        = htons(13); /* daytime server */
 
-	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+    Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));
 
-	Listen(listenfd, LISTENQ);
+    Listen(listenfd, LISTENQ);
 
-	for ( ; ; ) {
-		connfd = Accept(listenfd, (SA *) NULL, NULL);
+    for (;;) {
+        connfd = Accept(listenfd, (SA *)NULL, NULL);
 
         ticks = time(NULL);
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        Write(connfd, buff, strlen(buff));
+        for (size_t i = 0; i < strlen(buff); i++) {
+            Write(connfd, buff + i, 1);
+        }
 
-		Close(connfd);
-	}
+        Close(connfd);
+    }
 }

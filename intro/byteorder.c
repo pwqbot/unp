888665d1f1804e3c8be9cd863241ffd146c256f7
pwqbot <ding00000804@gmail.com>
2022-10-27 17:03:25 +0800

    ch3

--- intro/byteorder.c
+++ intro/byteorder.c
@@ -1,24 +1,42 @@
-#include	"unp.h"
+#include "unp.h"
+#include <netinet/in.h>
 
-int
-main(int argc, char **argv)
-{
-	union {
-	  short  s;
-      char   c[sizeof(short)];
+struct kk {
+    unsigned char a : 1;
+    unsigned char b : 8;
+    unsigned char c : 8;
+    unsigned char d : 8;
+};
+
+int main(int argc, char **argv) {
+    union {
+        short s;
+        char  c[sizeof(short)];
     } un;
 
-	un.s = 0x0102;
-	printf("%s: ", CPU_VENDOR_OS);
-	if (sizeof(short) == 2) {
-		if (un.c[0] == 1 && un.c[1] == 2)
-			printf("big-endian\n");
-		else if (un.c[0] == 2 && un.c[1] == 1)
-			printf("little-endian\n");
-		else
-			printf("unknown\n");
-	} else
-		printf("sizeof(short) = %d\n", sizeof(short));
+    un.s = 0x0102;
+    struct kk     stdData;
 
-	exit(0);
+    unsigned int x = 1;
+    memcpy(&stdData, &x, sizeof(stdData));
+    printf("%d, %d, %d , %d\n", stdData.a, stdData.b, stdData.c, stdData.d);
+
+    x = 30;
+    memcpy(&stdData, &x, sizeof(stdData));
+    printf("%d, %d, %d , %d\n", stdData.a, stdData.b, stdData.c, stdData.d);
+
+    printf("%s: ", CPU_VENDOR_OS);
+    if (sizeof(short) == 2) {
+        if (un.c[0] == 1 && un.c[1] == 2) {
+            printf("big-endian\n");
+        } else if (un.c[0] == 2 && un.c[1] == 1) {
+            printf("little-endian\n");
+        } else {
+            printf("unknown\n");
+        }
+    } else {
+        printf("sizeof(short) = %lu\n", sizeof(short));
+    }
+
+    exit(0);
 }
--- lib/readn.c
+++ lib/readn.c
@@ -1,37 +1,35 @@
 /* include readn */
-#include	"unp.h"
+#include "unp.h"
 
-ssize_t						/* Read "n" bytes from a descriptor. */
-readn(int fd, void *vptr, size_t n)
-{
-	size_t	nleft;
-	ssize_t	nread;
-	char	*ptr;
+ssize_t /* Read "n" bytes from a descriptor. */
+readn(int fd, void *vptr, size_t n) {
 
-	ptr = vptr;
-	nleft = n;
-	while (nleft > 0) {
-		if ( (nread = read(fd, ptr, nleft)) < 0) {
-			if (errno == EINTR)
-				nread = 0;		/* and call read() again */
-			else
-				return(-1);
-		} else if (nread == 0)
-			break;				/* EOF */
+    char  *ptr   = vptr;
+    size_t nleft = n;
+    while (nleft > 0) {
+        ssize_t nread = read(fd, ptr, nleft);
+        if (nread < 0) {
+            if (errno == EINTR) {
+                nread = 0; /* and call read() again */
+            } else {
+                return (-1);
+            }
+        } else if (nread == 0) {
+            break; /* EOF */
+        }
 
-		nleft -= nread;
-		ptr   += nread;
-	}
-	return(n - nleft);		/* return >= 0 */
+        nleft -= nread;
+        ptr += nread;
+    }
+    return (n - nleft); /* return >= 0 */
 }
 /* end readn */
 
-ssize_t
-Readn(int fd, void *ptr, size_t nbytes)
-{
-	ssize_t		n;
+ssize_t Readn(int fd, void *ptr, size_t nbytes) {
+    ssize_t n = 0;
 
-	if ( (n = readn(fd, ptr, nbytes)) < 0)
-		err_sys("readn error");
-	return(n);
+    if ((n = readn(fd, ptr, nbytes)) < 0) {
+        err_sys("readn error");
+    }
+    return (n);
 }
